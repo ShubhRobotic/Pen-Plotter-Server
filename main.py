bd412fb1fc6388691a00319d296834094fbb598a
ShubhRobotic <118299672+ShubhRobotic@users.noreply.github.com>
2022-11-20 21:11:24 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,38 +1,41 @@
-import asyncio
+from flask import Flask, redirect, url_for, render_template, request, Response, flash
+import requests
 import os
+import asyncio
 import subprocess
-
-from flask import Flask, redirect, render_template, request, url_for
+import sys
 
 app = Flask(__name__)
 
-   # async def run_cli():
-   # subprocess.run(["/home/pi/UniversalGcodeSender/start-cli.sh","--port /dev/ttyUSB0 --baud 115200 -daemon --controller GRBL"])
+# async def run_cli():
+# subprocess.run(["/home/pi/UniversalGcodeSender/start-cli.sh","--port /dev/ttyUSB0 --baud 115200 -daemon --controller GRBL"])
+# if request.method == 'POST':
+#  if request.form.get['submit_button'] == 'Connect':
+#
+#  elif request.form.get['submit_button'] == 'Home':
+#     subprocess.call(["/home/pi/my_flask/UI_Buttons_Bash/Homing.sh"], shell=True)
+#  else:
+#        pass # unknown
+#  elif request.method == 'GET':
 
-@app.route("/")
+
+@app.route('/')
 def home():
-    return render_template("index.html")
+    return render_template('base.html')
 
-@app.route("/login",methods=["POST","GET"])
-def login():
-    if request.method == "POST":
-        user = request.form["nm"]
-        return redirect(url_for("user",usr=user))
-    else:
-        return render_template("login.html")
 
-@app.route("/<usr>")
-def user(usr):
-   # asyncio.run(run_cli())
-    return "<h1>{usr}</h1>"
+@app.route("/control/", methods=['POST'])
+def control():
+    if request.method == 'POST':
+        subprocess.run(
+            ["/home/pi/my_flask/UI_Buttons_Bash/Connect.sh"], shell=True)
+    return "render_template('base.html')"
+
 
 @app.route("/convert")
 def convert():
     return render_template("convert.html")
 
-@app.route("/control")
-def control():
-    return render_template("control.html")
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
