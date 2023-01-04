834466917b66cd2a52485e19c49685adb3e805ed
ShubhRobotic <118299672+ShubhRobotic@users.noreply.github.com>
2023-01-04 12:56:37 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -6,11 +6,16 @@ import subprocess
 import os
 from wtforms.validators import InputRequired
 from PIL import Image
+import svgutils.transform as sg
+import cairosvg
 
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Gooseberry'
 app.config['UPLOAD_FOLDER'] = 'static/files'
+app.config['GCODE_FOLDER'] = 'static/gcodes'
+
+
 
 
 class UploadFileForm(FlaskForm):
@@ -26,23 +31,45 @@ def home():
         file = form.file.data
         file.filename = "Print.jpg"
         file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['UPLOAD_FOLDER'],secure_filename(file.filename)))
-        # resize image using 
-        img = Image.open('./static/files/Print.jpg') # Open image
-        img = img.resize((150, 100)) # Resize image
-        img.save('./static/files/foo.jpg') # Save resized image
+        img = Image.open('static/files/Print.jpg')
+        img_resize_lanczos = img.resize ((200,200), Image.LANCZOS)
+        img_resize_lanczos.save('static/files/resized.jpg') 
+        cmd1 = ['convert', 'static/files/resized.jpg', 'static/files/Run.svg']
+        subprocess.call(cmd1,shell=False)
     return render_template('base.html', form=form)
+@app.route('/upload', methods=['GET','POST'])
+def upload():
+    form = UploadFileForm()
+    if form.validate_on_submit():
+        # Get and store file and rename file to Print.jpg
+        file = form.file.data
+        file.filename = "Run.gcode"
+        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config['GCODE_FOLDER'],secure_filename(file.filename)))
+    return render_template('convert.html', form=form)
+
+@app.route("/connect/")
+def Connect():
+    subprocess.run(
+        ["/home/pi/my_flask/UI_Buttons_Bash/Connect.sh"], shell=True)
+    return redirect('/')
+
+@app.route("/Print/")
+def Print():
+    subprocess.run(
+        ["/home/pi/my_flask/UI_Buttons_Bash/Print.sh"], shell=True)
+    return redirect('/')
 
 @app.route("/homing/")
 def homing():
     subprocess.run(
         ["/home/pi/my_flask/UI_Buttons_Bash/Homing.sh"], shell=True)
-    return render_template('Control.html')
+    return redirect('/')
 
 @app.route("/reset_alarm/")
 def reset_alarm():
     subprocess.run(
         ["/home/pi/my_flask/UI_Buttons_Bash/Reset_Alarm.sh"], shell=True)
-    return render_template('Control.html')
+    return redirect('/')
 
 
 @app.route("/convert")
